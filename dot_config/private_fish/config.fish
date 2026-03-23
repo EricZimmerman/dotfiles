4fbec98a0f18d57824abcddc6d2c5ab079ff5659
Eric Zimmerman <saericzimmerman@gmail.com>
2026-03-23 11:52:38 -0400

    more stuff

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    alias cat="bat"
+    alias ssh="kitty +kitten ssh"
+
+    abbr -a --position anywhere -- --help '--help | bat -plhelp'
+    abbr -a --position anywhere -- -h '-h | bat -plhelp']
+
+    set -x EDITOR nvim
+    set -x VISUAL nvim
+
+    starship init fish | source
+end
