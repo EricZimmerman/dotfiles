59b0130c5313282778b78a9e14c46f9fd7fe26b6
Eric Zimmerman <saericzimmerman@gmail.com>
2026-03-26 15:27:25 -0400

    fish greeting

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    set -g fish_greeting = ""
+    set -g fish_greeting
 
     alias cat="bat"
     alias ssh="kitty +kitten ssh"
